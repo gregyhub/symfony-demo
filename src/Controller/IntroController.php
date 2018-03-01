ea003962d1f9c081f90c9e6520b7b3886ec22667
gregyhub <34886434+gregyhub@users.noreply.github.com>
2018-03-01 17:31:45 +0100

    smyfony > twig + doctrine

--- src/Controller/IntroController.php
+++ src/Controller/IntroController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use function dump;
 
 class IntroController extends Controller
 {
@@ -67,7 +70,83 @@ class IntroController extends Controller
      */
     public function twig()
     {
-        return $this->render('intro/twig.html.twig');
+        return $this->render('intro/twig.html.twig',
+                ['now' => new \DateTime()] /* le backslash pour chercher le datetime() a la racine des namespace / sinon il faut utiliser le use */
+                );
+    }
+    
+    /**
+     * @Route("/request")
+     * @param Request $request
+     * @return Reponse
+     */
+    public function request(Request $request) /* le typage request permet d'instancier l'objet request avec la requete http*/
+    {
+        // $_GET['test'], null si $get n'existe pas au lieu d'une notif undifine
+        dump($request->query->get('test'));
+        dump($request->query->get('test', 'valeur optionnelle par defaut'));
+        dump($request->query->all()); // //idem a var_dump($_GET)
+      
+        dump($request->getMethod());   //GET ou POST
+        
+        //isMethode vérifie si le getMethod est POST ou GET > si un formulaire à été  envoyé
+        if($request->isMethod('POST')){
+            dump($request->request->get('clic')); //récup ce qu'ily a dans le tableau post pour l'indice test
+            dump($request->request->all()); //idem a var_dump($_POST)
+        }
+        dump($request->get('clic')); // récupère l'info du GET ou du POST à l'indice donné, mais on ne sais pas si c'est un post ou un get
+        return $this->render('intro/request.html.twig'
+               
+                );
+    }
+    /**
+     * @Route("/response")
+     * @param Request $request
+     * @return Reponse
+     */
+    public function response(Request $request) 
+    {
+        if($request->query->get('found') == 'no'){
+            //pour envoyer une 404
+            throw new NotFoundHttpException();
+        }
+        if($request->query->get('redirect') == 'intro'){
+            //pour rediriger sur une nouvelle route
+            return $this->redirectToRoute('intro'); //intro c'est le "name" de la route intrp
+        }
+        if($request->query->has('name')){ //équivalent au isset($_GET['name'])
+            $name = $request->query->get('name');
+            return $this->redirectToRoute('app_intro_hello', array('name'=>$name)); 
+        }
+    }
+    /**
+     * @Route("/session")
+     * @param Request $request
+     * @return Reponse
+     */
+    public function session(Request $request) 
+    {
+        $session = $request->getSession();
+        $session->set('test', 'FOO'); //creer un indice 'test' dans $_SESSION avec la valeur FOO
+        $session->set('bar', 'BAR');
+        dump($session->get('test'));
+        dump($session->all());
+        $session->remove('bar');  // équivalemnt à unset($_SESSION['bar'])
+        dump($session->all());
+        
+        if($request->query->get('redirect')=='flash'){
+            //aoute un messageflash e type success
+            $session->getFlashBag()->add('success','message de confirmation');
+            return $this->redirectToRoute('app_intro_flash'); //intro c'est le "name" de la route intrp
+        }
+        
+        return $this->render('intro/session.html.twig');
+    }
+    /**
+     * @Route("/flash")
+     */
+    public function flash() {
+        return $this->render('intro/flash.html.twig');
     }
     /**
      * @Route("/boucle")
---
+++ src/Entity/Person.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ */
+//fait la relation entre class et les tables en bdd
+class Person
+{
+    /**
+     * @var int
+     * 
+     * @ORM\Id
+     * @ORM\GeneratedValue //autoincrément
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    
+    /**
+     * @var string
+     * @ORM\Column() //définit que cet attribut doit etre un champ dans la table > par défaut le champ est un varchar(255) NOT NULL
+     */
+    private $firstname;
+    /**
+     * @var string
+     * @ORM\Column()
+     */
+    private $lastname;
+    /**
+     * @var string
+     * @ORM\Column(unique=true) //précise que ce champ doit etre unique
+     */
+    private $email;
+    /**
+     * @var \Datetime
+     * @ORM\Column(type="date") //précise que ce champ et de type date
+     */
+    private $birthdate;
+    
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getFirstname() {
+        return $this->firstname;
+    }
+
+    public function getLastname() {
+        return $this->lastname;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getBirthdate(): \Datetime {
+        return $this->birthdate;
+    }
+
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this; //permet de chainer les appels de methodes grace à la case à cocher 'fluent'
+    }
+
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setBirthdate(\Datetime $birthdate) {
+        $this->birthdate = $birthdate;
+        return $this;
+    }
+
+
+
+
+}
